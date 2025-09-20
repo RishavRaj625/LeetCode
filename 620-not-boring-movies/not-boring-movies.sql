4175e7884f4d924cedfdcd1c4ef2406fcfc268d9
Rishav Raj <165421390+RishavRaj625@users.noreply.github.com>
2025-09-20 11:38:39 +0530

    Time: 211 ms (96.18%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,9 +1,14 @@
 # Write your MySQL query statement below
 
-select id, movie, description, rating
-from Cinema
+-- select id, movie, description, rating
+-- from Cinema
 -- where id % 2 = 1 AND description <> 'boring'
-group by id, description
-having id % 2 =1 AND description <> 'boring'
-order by rating desc;
+-- group by id, description
+-- having id % 2 =1 AND description <> 'boring'
+-- order by rating desc;
+
+SELECT id, movie, description, rating
+FROM Cinema
+WHERE id % 2 = 1 AND description <> 'boring'
+ORDER BY rating DESC;
 
